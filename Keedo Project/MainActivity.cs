24e8382b5ac684855730249303a5b4c31ac0aab2
daramithes <27282626@nescol.ac.uk>
2017-11-28 18:59:42 +0000

    Added basic database push integration when order is clicked, to use as a template for future work.

--- Keedo Project/MainActivity.cs
+++ Keedo Project/MainActivity.cs
@@ -1,18 +1,33 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Keedo_Project.Resources.Database;
+using System;
 
 namespace Keedo_Project
 {
     [Activity(Label = "Keedo_Project", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        private Button Clicker;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Clicker = (Button)FindViewById(Resource.Id.OrdersButtonMain);
+
+            Clicker.Click += ClickedItem;
+        }
+
+        async void ClickedItem(object sender, EventArgs e)
+        {
+            Test x = new Test();
+            await x.AddBook(this);
         }
     }
 }
---
+++ Keedo Project/Resources/Datamodel/TestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Keedo_Project.Resources.Datamodel
+{
+    class Books
+    {
+            //Model data so it matches my backend database
+            public string Id { get; set; }
+
+            public string BookName { get; set; }
+
+            public string BookPrice { get; set; }
+
+    }
+}
