22a33f9027e4545e665cb2568b802d3e485cad38
ANNA MCLEOD <1707029@rgu.ac.uk>
2017-12-04 21:14:04 +0000

    Added all orders page. Added Horizontal Line to previous UI.

--- Keedo Project/Resources/Resource.Designer.cs
+++ Keedo Project/Resources/Resource.Designer.cs
@@ -44,86 +44,125 @@ namespace Keedo_Project
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040019
-			public const int AuthorName = 2130968601;
+			// aapt resource value: 0x7f040026
+			public const int AuthorName = 2130968614;
 			
-			// aapt resource value: 0x7f040009
-			public const int BasketButtonMain = 2130968585;
+			// aapt resource value: 0x7f04000c
+			public const int BasketButtonMain = 2130968588;
 			
-			// aapt resource value: 0x7f04001e
-			public const int BookInfo = 2130968606;
+			// aapt resource value: 0x7f040007
+			public const int BookAuthor = 2130968583;
 			
-			// aapt resource value: 0x7f040017
-			public const int BookPrice = 2130968599;
+			// aapt resource value: 0x7f04002b
+			public const int BookInfo = 2130968619;
 			
-			// aapt resource value: 0x7f04000b
-			public const int BookPurchase = 2130968587;
+			// aapt resource value: 0x7f04001a
+			public const int BookOrder = 2130968602;
 			
-			// aapt resource value: 0x7f040005
-			public const int BookTitle = 2130968581;
+			// aapt resource value: 0x7f040008
+			public const int BookPrice = 2130968584;
 			
-			// aapt resource value: 0x7f04001d
-			public const int BuyBook = 2130968605;
+			// aapt resource value: 0x7f04000e
+			public const int BookPurchase = 2130968590;
 			
-			// aapt resource value: 0x7f04001b
-			public const int CollectDirect = 2130968603;
+			// aapt resource value: 0x7f040006
+			public const int BookTitle = 2130968582;
 			
-			// aapt resource value: 0x7f04001c
-			public const int CollectKeedo = 2130968604;
+			// aapt resource value: 0x7f040020
+			public const int BoughtFrom = 2130968608;
 			
-			// aapt resource value: 0x7f040010
-			public const int Email = 2130968592;
+			// aapt resource value: 0x7f04001f
+			public const int BoughtFromText = 2130968607;
+			
+			// aapt resource value: 0x7f04002a
+			public const int BuyBook = 2130968618;
+			
+			// aapt resource value: 0x7f040028
+			public const int CollectDirect = 2130968616;
+			
+			// aapt resource value: 0x7f040022
+			public const int CollectFrom = 2130968610;
+			
+			// aapt resource value: 0x7f040021
+			public const int CollectFromText = 2130968609;
+			
+			// aapt resource value: 0x7f040029
+			public const int CollectKeedo = 2130968617;
+			
+			// aapt resource value: 0x7f040013
+			public const int Email = 2130968595;
 			
 			// aapt resource value: 0x7f040004
 			public const int ExampleImage = 2130968580;
 			
-			// aapt resource value: 0x7f04000e
-			public const int FirstName = 2130968590;
+			// aapt resource value: 0x7f040011
+			public const int FirstName = 2130968593;
 			
-			// aapt resource value: 0x7f040014
-			public const int Grid = 2130968596;
+			// aapt resource value: 0x7f040017
+			public const int Grid = 2130968599;
 			
-			// aapt resource value: 0x7f04000f
-			public const int LastName = 2130968591;
+			// aapt resource value: 0x7f040012
+			public const int LastName = 2130968594;
 			
-			// aapt resource value: 0x7f040008
-			public const int LibraryHeader = 2130968584;
+			// aapt resource value: 0x7f04000b
+			public const int LibraryHeader = 2130968587;
 			
-			// aapt resource value: 0x7f040006
-			public const int MainToolbar = 2130968582;
-			
-			// aapt resource value: 0x7f040007
-			public const int MenuButtonMain = 2130968583;
+			// aapt resource value: 0x7f040009
+			public const int MainToolbar = 2130968585;
 			
 			// aapt resource value: 0x7f04000a
-			public const int OrdersButtonMain = 2130968586;
+			public const int MenuButtonMain = 2130968586;
 			
-			// aapt resource value: 0x7f040011
-			public const int Purchase = 2130968593;
+			// aapt resource value: 0x7f04001c
+			public const int OrderDetails = 2130968604;
+			
+			// aapt resource value: 0x7f04001e
+			public const int OrderID = 2130968606;
+			
+			// aapt resource value: 0x7f04001d
+			public const int OrderIDText = 2130968605;
+			
+			// aapt resource value: 0x7f04001b
+			public const int OrderLayout = 2130968603;
+			
+			// aapt resource value: 0x7f040024
+			public const int OrderTotal = 2130968612;
+			
+			// aapt resource value: 0x7f040023
+			public const int OrderTotalText = 2130968611;
+			
+			// aapt resource value: 0x7f04000d
+			public const int OrdersButtonMain = 2130968589;
+			
+			// aapt resource value: 0x7f040014
+			public const int Purchase = 2130968596;
+			
+			// aapt resource value: 0x7f040005
+			public const int PurchaseBookLayout = 2130968581;
+			
+			// aapt resource value: 0x7f04000f
+			public const int PurchaseLayout = 2130968591;
 			
 			// aapt resource value: 0x7f040001
 			public const int ReturnButton = 2130968577;
 			
-			// aapt resource value: 0x7f040013
-			public const int ScanButton = 2130968595;
-			
-			// aapt resource value: 0x7f040012
-			public const int SearchBar = 2130968594;
-			
 			// aapt resource value: 0x7f040016
-			public const int TestText = 2130968598;
+			public const int ScanButton = 2130968598;
 			
-			// aapt resource value: 0x7f04000d
-			public const int UserDetails = 2130968589;
+			// aapt resource value: 0x7f040015
+			public const int SearchBar = 2130968597;
+			
+			// aapt resource value: 0x7f040019
+			public const int TestText = 2130968601;
+			
+			// aapt resource value: 0x7f040010
+			public const int UserDetails = 2130968592;
 			
 			// aapt resource value: 0x7f040000
 			public const int layout = 2130968576;
 			
-			// aapt resource value: 0x7f04000c
-			public const int linearLayout1 = 2130968588;
-			
-			// aapt resource value: 0x7f04001a
-			public const int radioGroup1 = 2130968602;
+			// aapt resource value: 0x7f040027
+			public const int radioGroup1 = 2130968615;
 			
 			// aapt resource value: 0x7f040002
 			public const int tableLayout1 = 2130968578;
@@ -131,11 +170,11 @@ namespace Keedo_Project
 			// aapt resource value: 0x7f040003
 			public const int tableRow1 = 2130968579;
 			
-			// aapt resource value: 0x7f040018
-			public const int tableRow2 = 2130968600;
+			// aapt resource value: 0x7f040025
+			public const int tableRow2 = 2130968613;
 			
-			// aapt resource value: 0x7f040015
-			public const int testImage = 2130968597;
+			// aapt resource value: 0x7f040018
+			public const int testImage = 2130968600;
 			
 			static Id()
 			{
@@ -151,16 +190,22 @@ namespace Keedo_Project
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int BuyBook = 2130837504;
+			public const int AllOders = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Main = 2130837505;
+			public const int BuyBook = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Model = 2130837506;
+			public const int Main = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Product = 2130837507;
+			public const int Model = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int OrderDetails = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Product = 2130837509;
 			
 			static Layout()
 			{
